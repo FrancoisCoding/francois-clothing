c780fd663b93a4a3612d6e3d923e802001ca5f15
Isaiah <francoisisaiaht@yahoo.com>
2021-08-24 14:24:57 -0400

    Updated sections, added more routes

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -8,34 +8,39 @@ const Directory = () => {
       title: "hats",
       imageUrl: "https://i.imgur.com/djAGJc9.jpg",
       id: 1,
+      linkUrl: "hats",
     },
     {
       title: "jackets",
       imageUrl: "https://i.imgur.com/t19tSfw.jpg",
       id: 2,
+      linkUrl: "jackets",
     },
     {
       title: "sneakers",
       imageUrl: "https://i.imgur.com/2LlAEsp.jpg",
       id: 3,
+      linkUrl: "sneakers",
     },
     {
       title: "womens",
       imageUrl: "https://i.imgur.com/3fpOpsf.jpg?2",
       size: "large",
       id: 4,
+      linkUrl: "womans",
     },
     {
       title: "mens",
       imageUrl: "https://i.imgur.com/YKmuWiB.jpg",
       size: "large",
       id: 5,
+      linkUrl: "mens",
     },
   ]);
   return (
     <div className="directory-menu">
-      {sections.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      {sections.map(({ id, ...itemProps }) => (
+        <MenuItem key={id} {...itemProps} />
       ))}
     </div>
   );
