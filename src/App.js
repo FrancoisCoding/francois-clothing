854e8a240eeaf6a256fb4e18b86040efb39e29f3
Isaiah <francoisisaiaht@yahoo.com>
2021-08-24 20:52:30 -0400

    Added header component with link and custom logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import "./App.scss";
+import Header from "./components/Header/Header";
 import Routes from "./utils/Routes";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes />
     </div>
   );
